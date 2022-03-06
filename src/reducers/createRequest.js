8a851851194379cd373a86cc8db5bc93990e83f6
EunjiBaek <ejbaek@k-auction.com>
2022-03-06 11:29:44 +0900

    commit

--- src/reducers/createRequest.js
+++
@@ -1,6 +0,0 @@
-export const createRequest = (type) => {
-    const REQUEST = `${type}_REQUEST`;
-    const SUCCESS = `${type}_SUCCESS`;
-    const FAILURE = `${type}_FAILURE`;
-    return [REQUEST, SUCCESS, FAILURE];
-}
--- src/sagas/index.js
+++
@@ -1,9 +0,0 @@
-import { all, fork } from 'redux-saga/effects';
-
-import listSaga from './list';
-
-export default function* rootSaga() {
-  yield all([
-    fork(listSaga),
-  ]);
-}
